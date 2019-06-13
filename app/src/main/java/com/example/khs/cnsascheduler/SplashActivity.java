99ab8d97d9f96c027d9b7d34d1e01765cacd14bf
HihelloKohh <flute2606@gmail.com>
2019-06-13 17:49:34 +0900

    HI

--- app/src/main/java/com/example/khs/cnsascheduler/SplashActivity.java
+++ app/src/main/java/com/example/khs/cnsascheduler/SplashActivity.java
@@ -19,7 +19,7 @@ public class SplashActivity extends AppCompatActivity {
                 Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
                 SplashActivity.this.startActivity(mainIntent);
                 SplashActivity.this.finish();
-                //Love
+                //Love YOu
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
